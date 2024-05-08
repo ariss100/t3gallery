20b4fe36fad5d448af4d959c463353c2ee2f1f68
Aris <arisflpss@gmail.com>
2024-05-08 11:57:32 +0300

    added uploadthing and auto update with useRouter

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,6 +1,11 @@
+"use client";
 import { SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { UploadButton } from "~/utils/uploadthing";
 
 export function TopNav() {
+  const router = useRouter();
+
   return (
     <nav className="flex items-center justify-between w-full p-4 text-xl font-semibold border-b">
       <div>Gallery</div>
@@ -8,7 +13,13 @@ export function TopNav() {
         <SignedOut>
           <SignInButton />
         </SignedOut>
-        <UserButton />
+        <div className="flex justify-between gap-4">
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => router.refresh()}
+          />
+          <UserButton />
+        </div>
       </div>
     </nav>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,33 @@
 import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
+import "@uploadthing/react/styles.css";
+import { UploadButton } from "@uploadthing/react";
 
-export default async function HomePage() {
+async function Images() {
   const mockImages = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
+
+  return (
+    <div className="flex flex-wrap gap-4">
+      {mockImages.map((image) => (
+        <div key={image.id} className="w-48">
+          <img
+            src={image.url}
+            alt={image.name}
+            className="w-48 h-48 object-cover"
+          />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
   return (
     <main>
-      <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
-            <img
-              src={image.url}
-              alt={image.name}
-              className="w-48 h-48 object-cover"
-            />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <Images />
     </main>
   );
 }
